25af3a12b3ad4acd82ece24d19cd5d25c8c0ca83
omkar-103 <142158418+omkar-103@users.noreply.github.com>
2026-07-16 09:10:03 +0530

    react-day-0

---
+++ day-0/script.js
@@ -0,0 +1,28 @@
+// let obj = {
+//     name:"rahul",
+//     age:89,
+//     address:"rahul nagar",
+//     company:"google"
+// };
+
+// let fn = ({name, company}) => {
+//     console.log(name, company);
+// };
+
+// fn(obj);
+
+
+
+// Q2
+// let a = 990;
+
+// let print = () => {
+//     console.log(a);
+// };
+
+// let ab = () => {
+//     let a = 80;
+//     print();
+// };
+
+// ab();
